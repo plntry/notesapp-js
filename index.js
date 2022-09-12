b045ccbb25bd513511e5df6516476f864f4fb0c8
Oksana Nedilko <nedilko7b@gmail.com>
2022-09-12 21:51:35 +0300

    Fixed extracting date from the content & added to the localstorage data on default

--- index.js
+++ index.js
@@ -9,6 +9,57 @@ const notesContentInput = document.querySelector('.notes-content-input input');
 const selectedCategory = document.getElementById('categoryList');
 
 const notesTable = document.querySelector('.notes-table');
+localStorage.setItem('notes-list', JSON.stringify([{
+    name: 'Shopping list',
+    createdAt: 'April 20, 2022',
+    category: 'Task',
+    content: 'Tomatoes, bread',
+    dates: ''
+},
+{
+    name: 'New Feature',
+    createdAt: 'July 14, 2022',
+    category: 'Idea',
+    content: 'Implement new feature to the project 10/10/2022',
+    dates: '10/10/2022'
+},
+{
+    name: 'New project',
+    createdAt: 'August 18, 2022',
+    category: 'Task',
+    content: 'Create new project',
+    dates: ''
+},
+{
+    name: 'Help mother',
+    createdAt: 'September 10, 2022',
+    category: 'Task',
+    content: 'Help mother with cooking for the party on 11/09/2022 and 12/09/2022',
+    dates: '11/09/2022, 12/09/2022'
+},
+{
+    name: 'Love to be Ukrainian',
+    createdAt: 'April 20, 2022',
+    category: 'Random Thought',
+    content: 'Glory to Ukraine!',
+    dates: ''
+},
+{
+    name: 'Save Earth, save people',
+    createdAt: 'April 20, 2022',
+    category: 'Idea',
+    content: 'Take part in some events for supporting peace and happiness on our planet',
+    dates: ''
+},
+{
+    name: 'Homework at the university',
+    createdAt: 'April 20, 2022',
+    category: 'Task',
+    content: 'Doing my homework',
+    dates: ''
+},
+]));
+
 const notes = JSON.parse(localStorage.getItem('notes-list'));
 
 const addNoteForm = document.getElementById('addNoteForm');
@@ -23,7 +74,7 @@ addNoteForm.addEventListener('submit', event => {
     let noteCategory = selectedCategory.value;
     let noteContent = notesContentInput.value;
 
-    let regexp = /[0-9]{2}[\-/ \.][0-9]{2}[\-/ \.][0-9]{4}/g;
+    let regexp = /[0-9]{1,2}[\-/ \.][0-9]{1,2}[\-/ \.][0-9]{4}/g;
     let dateMatch = Array.from(noteContent.matchAll(regexp)).join(', ');
     console.log(dateMatch);
 
