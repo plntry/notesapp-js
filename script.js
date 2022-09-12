ee690e98f786fe59eed0e6a8daf1378ed7050553
Oksana Nedilko <nedilko7b@gmail.com>
2022-09-12 14:15:51 +0300

    Added possibility to add new note and see it in the table

--- script.js
+++ script.js
@@ -1,6 +1,62 @@
-let modal = document.getElementById("myModal");
-let openModalbtn = document.getElementById("myBtn");
-let closeModalspan = document.getElementsByClassName("close")[0];
+let modal = document.getElementById('myModal');
+let openModalbtn = document.getElementById('myBtn');
+let closeModalspan = document.getElementsByClassName('close')[0];
+
+const notesInput = document.querySelector('.notes-input input'),
+notesTable= document.querySelector('.notes-table');
+
+let notes = JSON.parse(localStorage.getItem('notes-list'));
+
+function showNote () {
+    let noteElements = '';
+    notesTable.innerHTML = `<tr>
+                                <td>Name</td>
+                                <td>Created</td>
+                                <td>Category</td>
+                                <td>Content</td>
+                                <td>Dates</td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>`;
+
+    notes.forEach((note, id) => {
+        noteElements += `<tr>
+                            <td>${note.name}</td>
+                            <td>${note.createdAt}</td>
+                            <td>${note.category}</td>
+                            <td>${note.content}</td>
+                            <td>${note.dates}</td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>`
+    });
+    notesTable.innerHTML += noteElements;
+}
+showNote();
+
+notesInput.addEventListener('keyup', e=> {
+    let userNote = notesInput.value;
+    if (e.key == 'Enter' && userNote) {
+        if (!notes) {
+            notes = [];
+        }
+
+        notesInput.value = '';
+
+        let noteInfo = {
+            name: userNote,
+            createdAt: new Date(),
+            category: 'fd',
+            content: 'fds',
+            dates: 'dfs'
+        }
+        notes.push(noteInfo);
+        localStorage.setItem('notes-list', JSON.stringify(notes));
+        showNote();
+    }
+})
 
 // When the user clicks the button, open the modal 
 openModalbtn.onclick = function() {
